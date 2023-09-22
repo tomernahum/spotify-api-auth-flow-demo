be2b48c1058e4c9ba1da34eb6213b4ffaf5bc450
tomernahum <git@tomer.nahum.org>
2023-09-22 15:46:48 -0400

    oops

--- src/env_vars.ts
+++ src/env_vars.ts
@@ -1,7 +1,7 @@
 
 //not sure clientId is secret, I think not, I guess what stops people from impersonating me is the redirectURL
 
-export const clientId = import.meta.env.VITE_CLIENT_ID ||;  
+export const clientId = import.meta.env.VITE_CLIENT_ID;
 export const redirectUri = import.meta.env.VITE_REDIRECT_URI;
 
 if (!clientId) {
