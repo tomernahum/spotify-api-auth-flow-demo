021283678c76fd1695d2df22b1e8204c59909bd3
tomernahum <git@tomer.nahum.org>
2023-09-22 16:20:44 -0400

    revert last: good enough for now

--- src/env_vars.ts
+++ src/env_vars.ts
@@ -1,10 +1,8 @@
+//TODO: be able to derive redirectUri from the base URL (for instance the CF_PAGES_URL env var)
 
 //not sure clientId is secret, I think not, I guess what stops people from impersonating me is the redirectURL
-
 export const clientId = import.meta.env.VITE_CLIENT_ID;
-export const redirectUri = import.meta.env.CF_PAGES_URL || import.meta.env.VITE_REDIRECT_URI; 
-//this needs to redone righter, I just can't be bothered i just want this to work. theres something called meta.env.base_url for instance
-
+export const redirectUri = import.meta.env.VITE_REDIRECT_URI;
 
 if (!clientId) {
     throw new Error("env.VITE_CLIENT_ID not defined");
